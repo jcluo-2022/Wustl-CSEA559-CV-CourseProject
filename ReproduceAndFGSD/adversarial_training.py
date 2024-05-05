33f4112733cc1887fcbd5983300c7baabdbc381b
ljc <jincheng.luo@wustl.edu>
2024-05-05 09:34:09 -0500

    Add the code of adversarial_training

---
+++ ReproduceAndFGSD/adversarial_training.py
@@ -0,0 +1,175 @@
+from data_aug import get_data_augmentations
+from dataset import load_train_data, load_val_data, load_val_data_origin
+from engine import train, validate, adversarial_training
+from log import create_logger
+from math import ceil
+from os import get_terminal_size
+from scaler import NativeScaler
+from throughput import throughput
+from timm.loss.cross_entropy import SoftTargetCrossEntropy
+from timm.models import create_model
+
+import argparse, sys, torch
+import torch.nn as nn
+import torch.optim as optim
+
+
+def parse_args():
+    parser = argparse.ArgumentParser('Vision Transformer training and evaluation script', add_help=False)
+    parser.add_argument('--model', type=str, required=True, choices=['vit', 'deit', 'swin', 'cait', 'beit'],
+                        help='vit: ViT-L/16, '
+                             'deit: DeiT-B/16 distilled, '
+                             'swin: Swin-L, '
+                             'cait: CaiT-S36')
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--train', action='store_true', help='Train a model')
+    group.add_argument('--evaluate', type=str, help='Evaluate a trained model at the given file path')
+    parser.add_argument('--throughput', action='store_true', help='Test throughput only')
+    parser.add_argument('--resume', type=str, help='Resume training')
+
+    # data augmentation
+    parser.add_argument('--mixup', action='store_true', default=True)
+    parser.add_argument('--no-mixup', action='store_false', dest='mixup', help='Disable mixup')
+    parser.add_argument('--cutmix', action='store_true', default=True)
+    parser.add_argument('--no-cutmix', action='store_false', dest='cutmix', help='Disable cutmix')
+    parser.add_argument('--randerase', action='store_true', default=True)
+    parser.add_argument('--no-randerase', action='store_false', dest='randerase', help='Disable random erasing')
+    parser.add_argument('--randaug', action='store_true', default=False)
+
+    # optimizer
+    parser.add_argument('--optim', type=str, default='adamw', choices=['adamw', 'sgd'])
+    parser.add_argument('--nesterov', action='store_true', default=True, help='Use nesterov momentum for SGD')
+    parser.add_argument('--no-nesterov', action='store_false', dest='nesterov', help='Disable nesterov')
+    parser.add_argument('--lr', type=float, default=1e-3, help='Learning rate for optimizer')
+    parser.add_argument('--wd', type=float, default=0.05, help='Weight decay for optimizer')
+
+    parser.add_argument('--label-smooth', type=float, default=0.1, help='Label smoothing percent')
+
+    args, _ = parser.parse_known_args()
+
+    return args
+
+
+def load_model(model_name, evaluate):
+    if model_name == 'cait':
+        model = create_model('cait_s36_384', pretrained=True, drop_path_rate=0.1)
+        batch_size = 128
+    elif model_name == 'deit':
+        model = create_model('deit_base_distilled_patch16_384', pretrained=True, drop_path_rate=0.1)
+        batch_size = 64
+    elif model_name == 'swin':
+        model = create_model('swin_large_patch4_window12_384', pretrained=True, drop_path_rate=0.1)
+        batch_size = 4
+    elif model_name == 'vit':
+        model = create_model('vit_large_patch16_384', pretrained=True, drop_path_rate=0.1)
+        batch_size = 64
+    else:
+        logger.error('Invalid model name, please use either cait, deit, swin, or vit')
+        sys.exit(1)
+
+    for param in model.parameters():
+        param.requires_grad = False
+    model.reset_classifier(num_classes=200)
+
+    if evaluate:
+        if evaluate.startswith('https'):
+            checkpoint = torch.hub.load_state_dict_from_url(evaluate)
+        else:
+            checkpoint = torch.load(evaluate)
+        model.load_state_dict(checkpoint['model_state_dict'])
+
+    return model, batch_size
+
+
+def load_optimizer(args, model):
+    if args.optim == 'adamw':
+        optimizer = optim.AdamW(model.parameters(), lr=args.lr, weight_decay=args.wd)
+    elif args.optim == 'sgd':
+        optimizer = optim.SGD(model.parameters(), lr=args.lr, weight_decay=args.wd, momentum=0.9,
+                              nesterov=args.nesterov)
+    else:
+        logger.error('Invalid optimizer name, please use either adamw or sgd')
+        sys.exit(1)
+
+    return optimizer
+
+
+if __name__ == '__main__':
+    logger = create_logger()
+    args = parse_args()
+
+    torch.cuda.empty_cache()
+    torch.backends.cudnn.benchmark = True
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model, batch_size = load_model(args.model, args.evaluate)
+    model = model.to(device)
+    logger.info(f"Parameters: {sum(p.numel() for p in model.parameters())}")
+
+    true_batch_size = 128
+    update_freq = true_batch_size // batch_size
+    img_size = 384
+    val_loader = load_val_data_origin(img_size, batch_size if not args.throughput else 32)
+
+    if args.throughput:
+        logger.info(f"Testing throughput of {args.model}")
+        throughput(val_loader, model, logger)
+    elif args.evaluate:
+        logger.info(f"Evaluating {args.model} on validation set")
+        val_loss, top_1_val_acc, top_5_val_acc = validate(model, device, val_loader, -1)
+        logger.info(f"Top 1 Validation Accuracy: {top_1_val_acc}\tTop 5 Validation Accuracy: {top_5_val_acc}")
+    elif args.train:
+        randaug_magnitude = 9 if args.randaug else 0
+        start_epoch = 0
+        epochs = 30
+
+        train_loader = load_train_data(img_size, randaug_magnitude, batch_size)
+
+        mixup, random_erase = get_data_augmentations(
+            args.label_smooth,
+            en_mixup=args.mixup,
+            en_cutmix=args.cutmix,
+            en_randerase=args.randerase
+        )
+
+        if mixup:
+            loss_fn = SoftTargetCrossEntropy()
+        else:
+            loss_fn = nn.CrossEntropyLoss(label_smoothing=args.label_smooth)
+
+        optimizer = load_optimizer(args, model)
+        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer,
+                                                         T_max=ceil(len(train_loader) / update_freq) * epochs)
+        loss_scaler = NativeScaler()
+
+        if args.resume:
+            checkpoint = torch.load(args.resume, map_location=device)
+            start_epoch = checkpoint['start_epoch'] + 1
+            model.load_state_dict(checkpoint['model_state_dict'])
+
+            optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+            scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+            loss_scaler.load_state_dict(checkpoint['loss_scaler_state_dict'])
+            del checkpoint
+
+        logger.info(f"Start training {args.model} at epoch {start_epoch + 1}")
+        logger.info(f"Current lr: {optimizer.param_groups[0]['lr']}")
+
+        for i in range(start_epoch, epochs):
+            logger.info(f"Epoch {i + 1}")
+            train_loss = adversarial_training(model, loss_fn, optimizer, device, train_loader, scheduler, loss_scaler, update_freq,
+                               mixup, random_erase)
+
+            val_loss, top_1_val_acc, top_5_val_acc = validate(model, device, val_loader, i,
+                                                              can_visualize=False)
+
+            torch.save({
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'scheduler_state_dict': scheduler.state_dict(),
+                'loss_scaler_state_dict': loss_scaler.state_dict(),
+                'start_epoch': i,
+            }, f'models/{args.model}/epoch{i + 1}_top1_acc:{top_1_val_acc}_top5_acc{top_5_val_acc}.pth')
+
+            logger.info(f"Current lr: {optimizer.param_groups[0]['lr']}")
+            logger.info(f"Top 1 Validation Accuracy: {top_1_val_acc}\tTop 5 Validation Accuracy: {top_5_val_acc}")
--- ReproduceAndFGSD/engine.py
+++ ReproduceAndFGSD/engine.py
@@ -5,10 +5,12 @@ from visualize import visualize
 import torch, random
 import torch.nn as nn
 
-def train(model, loss_fn, optimizer, device, train_loader, scheduler, loss_scaler, update_freq, mixup=None, random_erase=None):
+
+def train(model, loss_fn, optimizer, device, train_loader, scheduler, loss_scaler, update_freq, mixup=None,
+          random_erase=None):
     loss_ema = -1
     iterator = tqdm(train_loader, total=int(len(train_loader)))
-    
+
     update = False
 
     # deit finetunes in eval mode
@@ -19,14 +21,14 @@ def train(model, loss_fn, optimizer, device, train_loader, scheduler, loss_scale
 
     for i, (x, y) in enumerate(iterator):
         x, y = x.to(device, non_blocking=True), y.to(device, non_blocking=True)
-        
+
         if mixup:
             x, y = mixup(x, y)
 
         if random_erase:
             x = random_erase(x)
 
-        update = True if (i+1) % update_freq == 0 or i + 1 == len(train_loader) else False
+        update = True if (i + 1) % update_freq == 0 or i + 1 == len(train_loader) else False
         with torch.cuda.amp.autocast():
             pred = model(x)
             loss = loss_fn(pred, y)
@@ -40,11 +42,89 @@ def train(model, loss_fn, optimizer, device, train_loader, scheduler, loss_scale
         with torch.no_grad():
             if loss_ema < 0:
                 loss_ema = loss.item()
-            loss_ema = loss_ema*0.99 + loss.item()*0.01
+            loss_ema = loss_ema * 0.99 + loss.item() * 0.01
 
             iterator.set_postfix(train_loss=loss_ema)
-    
+
     return loss_ema
+
+
+img_size = 384
+batch_size = 4
+means = torch.tensor([0.485, 0.456, 0.406], dtype=torch.float32)
+stds = torch.tensor([0.229, 0.224, 0.225], dtype=torch.float32)
+min_vals = ((0 - means) / stds).view(1, 3, 1, 1).expand(batch_size, 3, img_size, img_size).to("cuda:0")
+max_vals = ((1 - means) / stds).view(1, 3, 1, 1).expand(batch_size, 3, img_size, img_size).to("cuda:0")
+
+
+def fgsm_attack(image, gradient, epsilon=4):
+    # Get the direction of the gradient
+    sign_data_grad = gradient.sign()
+    #
+    perturbed_image = image + epsilon * sign_data_grad
+    # clip the value into valid range
+    perturbed_image = torch.max(torch.min(perturbed_image, max_vals), min_vals)
+    return perturbed_image
+
+
+def adversarial_training(model, loss_fn, optimizer, device, train_loader, scheduler, loss_scaler, update_freq,
+                         mixup=None,
+                         random_erase=None):
+    loss_ema = -1
+    iterator = tqdm(train_loader, total=int(len(train_loader)))
+
+    update = False
+
+    # deit finetunes in eval mode
+    if type(model).__name__ == 'VisionTransformerDistilled':
+        model.eval()
+    else:
+        model.train()
+
+    for i, (x, y) in enumerate(iterator):
+        x, y = x.to(device, non_blocking=True), y.to(device, non_blocking=True)
+
+        if mixup:
+            x, y = mixup(x, y)
+
+        if random_erase:
+            x = random_erase(x)
+
+        update = True if (i + 1) % update_freq == 0 or i + 1 == len(train_loader) else False
+        with torch.cuda.amp.autocast():
+            pred = model(x)
+            loss = loss_fn(pred, y)
+        loss_scaler(loss, optimizer, update_grad=False)
+
+        # compute the gradient of the training image
+        x_adv = x.detach().clone()
+        x_adv.requires_grad = True
+        with torch.cuda.amp.autocast():
+            pred_adv = model(x_adv)
+            loss_adv = loss_fn(pred_adv, y)
+        loss_scaler(loss_adv, optimizer, update_grad=False)
+
+        # Use adversarial image to train
+        adv_x = fgsm_attack(image=x_adv, gradient=x_adv.grad)
+        with torch.cuda.amp.autocast():
+            pred_adv = model(adv_x)
+            loss_adv = loss_fn(pred_adv, y)
+        loss_scaler(loss_adv, optimizer, update_grad=update)
+
+        if update:
+            for param in model.parameters():
+                param.grad = None
+            scheduler.step()
+
+        with torch.no_grad():
+            if loss_ema < 0:
+                loss_ema = loss.item()
+            loss_ema = loss_ema * 0.99 + loss.item() * 0.01
+
+            iterator.set_postfix(train_loss=loss_ema)
+
+    return loss_ema
+
 
 def validate(model, device, val_loader, epoch, can_visualize=False):
     iterator = tqdm(val_loader, total=int(len(val_loader)))
@@ -62,14 +142,14 @@ def validate(model, device, val_loader, epoch, can_visualize=False):
 
             if loss_ema < 0:
                 loss_ema = loss.item()
-            loss_ema = loss_ema*0.99 + loss.item()*0.01
+            loss_ema = loss_ema * 0.99 + loss.item() * 0.01
 
             iterator.set_postfix(val_loss=loss_ema)
 
             if can_visualize and random.random() < 0.015:
                 visualize(x, pred, y, epoch)
 
-            top1, top5 = accuracy(pred, y, topk=(1,5))
+            top1, top5 = accuracy(pred, y, topk=(1, 5))
             acc1_meter.update(top1.item(), y.size(0))
             acc5_meter.update(top5.item(), y.size(0))
 
